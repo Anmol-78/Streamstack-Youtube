59d96611a7373483e7a3a6dce59cbaa2f81c04c6
Anmol Goyal <anmolgoyal7817@gmai.com>
2025-08-05 16:07:35 +0530

    removing cors

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -5,12 +5,7 @@ import morgan from "morgan";
 
 const app = express()
 
-app.use(
-    cors({
-        origin: process.env.FRONTEND_URL,
-        credentials: true,
-    })
-);
+
 
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({ extended: true, limit: "16kb" }))
